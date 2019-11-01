b5177a5cf8eaaad5638eb704590ff61481567092
Russell Smith <ukd1@users.noreply.github.com>
2019-10-31 22:46:43 -0700

    Update members

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ex-github or not
 
 This is a really simple script to track who publically joins / leaves github. It's possibly interesting due to the current sitation with ICE. Updates ~every 12h.
+
+We started with 214 publically listed folks. The current count as of 2019-10-31 is 214.
--- main.rb
+++ main.rb
@@ -6,8 +6,16 @@ client.auto_paginate = true
 
 `rm members/*.json`
 
+cnt = 0
 client.org_members('github').each do |member|
+  cnt += 1
   File.write("members/#{member.login}.json", JSON.pretty_generate(member.to_h))
 end
 
-`git add members && git commit -am "Update members" && git push origin master`
+File.write("README.md", "# ex-github or not
+
+This is a really simple script to track who publically joins / leaves github. It's possibly interesting due to the current sitation with ICE. Updates ~every 12h.
+
+We started with 214 publically listed folks. The current count as of #{Date.today} is #{cnt}.")
+
+`git add README.md members && git commit -am "Update members" && git push origin master`
